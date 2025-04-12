f0338a96278d0361d53367fe25c0277c4310e759
Luana Silva Gonçalves <96505397+luanagoncalves28@users.noreply.github.com>
2025-04-11 22:42:53 -0300

    Delete infraestrutura/cleanup-dev.sh

--- infraestrutura/cleanup-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script para remover recursos do ambiente de desenvolvimento
-# Use: ./cleanup-dev.sh
-
-echo "Removendo recursos do ambiente de desenvolvimento..."
-cd terraform/ambientes/dev
-terraform destroy -auto-approve
-
-echo "Recursos removidos com sucesso!"
