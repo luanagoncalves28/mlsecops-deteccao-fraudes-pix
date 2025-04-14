b8f47a3b44df5dbe519b3f02999704095512966f
Luana Silva Gonçalves <96505397+luanagoncalves28@users.noreply.github.com>
2025-04-14 14:00:32 -0300

    Update variable.tf

--- infraestrutura/modules/vpc/variable.tf
+++ infraestrutura/modules/vpc/variable.tf
@@ -1,48 +1,16 @@
-############################################################
-# FILE: variable.tf
-# FOLDER: mlsecpix-infra/modules/vpc/
+##########################################################
+# FILE: variables.tf
+# MODULE: vpc
 # DESCRIPTION:
-#   Este arquivo define as variáveis necessárias para o
-#   módulo de VPC do projeto MLSecPix. Todos os parâmetros
-#   relacionados à configuração da rede (como project_id,
-#   region, vpc_name, subnet_name, cidr_subnet e enable_flow_logs)
-#   estão centralizados aqui, seguindo os princípios de Clean Code,
-#   evitando duplicações e facilitando a manutenção, o que é
-#   essencial para ambientes MLSecOps e para cumprir requisitos
-#   regulatórios.
-############################################################
+# Variáveis para o módulo de VPC e rede
+##########################################################
 
 variable "project_id" {
+  description = "ID do projeto GCP"
   type        = string
-  description = "ID do projeto GCP onde a VPC será criada."
 }
 
 variable "region" {
+  description = "Região onde será criada a subnet"
   type        = string
-  description = "Região padrão do GCP."
-  default     = "southamerica-east1"
-}
-
-variable "vpc_name" {
-  type        = string
-  description = "Nome desejado para a rede VPC."
-  default     = "mlsecpix-vpc"
-}
-
-variable "subnet_name" {
-  type        = string
-  description = "Nome desejado para a sub-rede."
-  default     = "mlsecpix-subnet"
-}
-
-variable "cidr_subnet" {
-  type        = string
-  description = "CIDR da sub-rede (ex.: 10.0.0.0/16)."
-  default     = "10.0.0.0/16"
-}
-
-variable "enable_flow_logs" {
-  type        = bool
-  description = "Habilitar ou não Flow Logs para auditoria (importante para compliance com BCB nº 403)."
-  default     = true
 }
