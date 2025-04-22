8376f6c106cf40e6c4edbd14962417a8c55f0dfa
luanagoncalves28e <lugonc.lga@gmail.com>
2025-04-21 22:28:44 -0300

    INFRA‑002f | drop obsolete canary output

--- iac/outputs.tf
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# iac/outputs.tf  – outputs do root
-###############################################################################
-
-output "canary_bucket_url" {
-  description = "URL do bucket canary criado para validação."
-  value       = google_storage_bucket.tf_canary.url
-}
