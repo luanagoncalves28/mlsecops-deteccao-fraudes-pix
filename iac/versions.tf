f1797d03f0cb496e6a9fd33658320fa1ce11da4d
luanagoncalves28e <lugonc.lga@gmail.com>
2025-04-22 15:25:41 -0300

    chore: remove duplicate required_providers block

--- iac/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.6"
-
-  required_providers {
-    google     = { source = "hashicorp/google",   version = "~> 5.10" }
-    databricks = { source = "databricks/databricks", version = "~> 1.35" }
-  }
-}
