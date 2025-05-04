0437540d16d8438915a66ba757717836f5f0b908
luanagoncalves28e <lugonc.lga@gmail.com>
2025-05-04 11:33:23 -0300

    fix(monitoring): unifica required_providers e remove duplicidade
    * Inclui provider grafana no bloco existente em modules/monitoring/main.tf
    * Remove modules/monitoring/terraform.tf para atender regra “apenas um required_providers por módulo”

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,18 +1,26 @@
 # Este arquivo serve como ponto de entrada para o módulo de monitoramento
-# Todos os recursos principais são definidos em outros arquivos mais específicos
+# Todos os recursos principais são definidos em arquivos específicos
 # (namespace.tf, prometheus.tf, grafana.tf, ml_metrics_exporter.tf)
 
-# Verificações de versão do provider Kubernetes
+###############################################################################
+# BLOCO ÚNICO de required_providers PARA TODO O MÓDULO
+###############################################################################
 terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.10.0"
     }
+    grafana = {
+      source  = "grafana/grafana"
+      version = ">= 1.40.0"
+    }
   }
 }
 
-# Configurações locais utilizadas em vários recursos
+###############################################################################
+# Locais compartilhados
+###############################################################################
 locals {
   common_labels = merge(var.labels, {
     "app.kubernetes.io/managed-by" = "terraform"
@@ -20,7 +28,7 @@ locals {
     "environment"                  = var.environment
     "project"                      = var.project_id
   })
-  
-  # Nomes de recursos padronizados
+
+  # Prefixo padronizado p/ recursos
   resource_prefix = "${var.project_id}-${var.environment}"
 }
