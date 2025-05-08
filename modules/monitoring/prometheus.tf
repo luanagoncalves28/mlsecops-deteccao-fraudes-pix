4b19ed24c445d56e7be9675d381d43602cc3885e
luanagoncalves28e <lugonc.lga@gmail.com>
2025-05-07 21:57:22 -0300

    atualizar este arquivo para incluir o Deployment e o Service do Prometheus.

--- modules/monitoring/prometheus.tf
+++ modules/monitoring/prometheus.tf
@@ -94,3 +94,102 @@ groups:
 EOF
   }
 }
+
+# Adicionando o Deployment do Prometheus
+resource "kubernetes_deployment" "prometheus" {
+  metadata {
+    name      = "prometheus"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    labels = {
+      app = "prometheus"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "prometheus"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "prometheus"
+        }
+      }
+
+      spec {
+        container {
+          name  = "prometheus"
+          image = "prom/prometheus:v2.45.0"
+          args  = ["--config.file=/etc/prometheus/prometheus.yml", "--storage.tsdb.retention.time=15d"]
+
+          port {
+            container_port = 9090
+          }
+
+          volume_mount {
+            name       = "config-volume"
+            mount_path = "/etc/prometheus"
+          }
+
+          volume_mount {
+            name       = "prometheus-data"
+            mount_path = "/prometheus"
+          }
+
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "2Gi"
+            }
+            requests = {
+              cpu    = "200m"
+              memory = "1Gi"
+            }
+          }
+        }
+
+        volume {
+          name = "config-volume"
+          config_map {
+            name = kubernetes_config_map.prometheus_config.metadata[0].name
+          }
+        }
+
+        volume {
+          name = "prometheus-data"
+          empty_dir {}
+        }
+      }
+    }
+  }
+}
+
+# Adicionando o Service do Prometheus
+resource "kubernetes_service" "prometheus_service" {
+  metadata {
+    name      = "prometheus-service"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    labels = {
+      app = "prometheus"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "prometheus"
+    }
+
+    port {
+      name        = "http"
+      port        = 9090
+      target_port = 9090
+    }
+
+    type = "ClusterIP"
+  }
+}
